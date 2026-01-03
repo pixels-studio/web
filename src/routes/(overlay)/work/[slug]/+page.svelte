cd76b39f701cc5eecd0d65739f8f7d4664fd02f0
pixels-studio <abhi@pixels.studio>
2026-01-03 23:34:58 +0530

    feat: Add back navigation to work detail page with chevron icon and escape key support.

--- src/routes/(overlay)/work/[slug]/+page.svelte
+++ src/routes/(overlay)/work/[slug]/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
+	import { cn } from '$lib/helpers/utils';
 	import type { PageData } from './$types';
+	import ChevronLeft from '$lib/assets/chevron-left.svg?raw';
+	import { onMount } from 'svelte';
 
 	let { data }: { data: PageData } = $props();
+
+	function goBack() {
+		history.back();
+	}
+
+	onMount(() => {
+		function handleKeydown(event: KeyboardEvent) {
+			if (event.key === 'Escape') {
+				goBack();
+			}
+		}
+
+		window.addEventListener('keydown', handleKeydown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeydown);
+		};
+	});
 </script>
 
 <svelte:head>
@@ -13,8 +34,23 @@
 	/>
 </svelte:head>
 
-<section class="z-50 flex min-h-dvh flex-1 items-center justify-center bg-base-0">
-	<div class="relative aspect-2/1 w-full overflow-hidden">
+<section
+	class={cn(
+		'z-50 flex min-h-dvh flex-1 items-center justify-center bg-base-0 p-5',
+		'md:p-10',
+		'lg:p-20'
+	)}
+>
+	<button
+		onclick={goBack}
+		class="fixed top-5 left-5 z-10 grid size-10 cursor-pointer place-items-center rounded-full bg-base-10/5 transition-all ease-smooth hover:scale-105 hover:bg-base-10/10 active:scale-98"
+	>
+		<span class="block size-6">
+			{@html ChevronLeft}
+		</span>
+	</button>
+
+	<div class="relative aspect-2/1 w-full max-w-[1512px] overflow-hidden">
 		{#if data.work.type === 'video'}
 			<video
 				src={data.work.cover}
