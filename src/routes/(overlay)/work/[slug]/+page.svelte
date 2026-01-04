700229f0e5985d52a9459512076e05d572decfd7
abhishek-samespace <abhishek.kambli@samespace.com>
2026-01-04 09:45:59 +0530

    refactor: Remove back button and simplify layout styling in work overlay. (#16)
    Co-authored-by: pixels-studio <abhi@pixels.studio>

--- src/routes/(overlay)/work/[slug]/+page.svelte
+++ src/routes/(overlay)/work/[slug]/+page.svelte
@@ -34,23 +34,8 @@
 	/>
 </svelte:head>
 
-<section
-	class={cn(
-		'z-50 flex min-h-dvh flex-1 items-center justify-center bg-base-0 p-5',
-		'md:p-10',
-		'lg:p-20'
-	)}
->
-	<button
-		onclick={goBack}
-		class="fixed top-5 left-5 z-10 grid size-10 cursor-pointer place-items-center rounded-full bg-base-10/5 transition-all ease-smooth hover:scale-105 hover:bg-base-10/10 active:scale-98"
-	>
-		<span class="block size-6">
-			{@html ChevronLeft}
-		</span>
-	</button>
-
-	<div class="relative aspect-2/1 w-full max-w-[1512px] overflow-hidden">
+<section class={cn('z-50 flex min-h-dvh flex-1 items-center justify-center bg-base-0 ')}>
+	<div class="relative aspect-2/1 w-full overflow-hidden">
 		{#if data.work.type === 'video'}
 			<video
 				src={data.work.cover}
