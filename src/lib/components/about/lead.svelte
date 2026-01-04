3aba97cafc4f8fd8b01b2e35b7f61f1d73eef5f3
pixels-studio <abhi@pixels.studio>
2026-01-04 12:55:58 +0530

    refactor: Simplify layout structure in lead component
    - Removed unnecessary div wrapper around the lead section for cleaner markup.
    - Maintained existing styling and structure while enhancing readability and maintainability.
    
    Co-authored-by: pixels-studio <abhi@pixels.studio>

--- src/lib/components/about/lead.svelte
+++ src/lib/components/about/lead.svelte
@@ -3,25 +3,23 @@
 	import { cn } from '$lib/helpers/utils';
 </script>
 
-<div>
-	<section class={cn('bg-base-0 px-4 pt-32 pb-8', 'md:pt-48 md:pb-12', 'lg:pt-64 lg:pb-16')}>
-		<div
-			class={cn(
-				'mx-auto grid w-full max-w-[1512px] grid-cols-6 gap-x-4',
-				'md:grid-cols-8',
-				'lg:grid-cols-12'
-			)}
-		>
-			<h1 class={cn('col-span-full text-3xl font-medium text-pretty')}>
-				<span class={cn('inline', 'md:block')}>A decade spent shaping products </span>
-				<span class={cn('inline text-base-5', 'md:block')}
-					>From early ideas to production systems.</span
-				>
-			</h1>
-		</div>
-	</section>
-
-	<div class={cn('col-span-full aspect-2/1 bg-base-1')}>
-		<enhanced:img src={HeadshotImage} alt="Abhishek Kambli" class="h-full w-full object-cover" />
+<section class={cn('bg-base-0 px-4 pt-32 pb-8', 'md:pt-48 md:pb-12', 'lg:pt-64 lg:pb-16')}>
+	<div
+		class={cn(
+			'mx-auto grid w-full max-w-[1512px] grid-cols-6 gap-x-4',
+			'md:grid-cols-8',
+			'lg:grid-cols-12'
+		)}
+	>
+		<h1 class={cn('col-span-full text-3xl font-medium text-pretty')}>
+			<span class={cn('inline', 'md:block')}>A decade spent shaping products </span>
+			<span class={cn('inline text-base-5', 'md:block')}
+				>From early ideas to production systems.</span
+			>
+		</h1>
 	</div>
+</section>
+
+<div class={cn('col-span-full aspect-2/1 bg-base-1')}>
+	<enhanced:img src={HeadshotImage} alt="Abhishek Kambli" class="h-full w-full object-cover" />
 </div>
