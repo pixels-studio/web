daa3502cf7a9733df2f474e1a80377f8a00a8e51
pixels-studio <abhi@pixels.studio>
2026-03-30 00:29:29 +0530

    added playground section

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -4,3 +4,11 @@ export const routes = [
   { label: "About", href: "/about" },
   { label: "Contact", href: "/contact" },
 ] as const;
+
+export const socials = [
+  { label: "Twitter", href: "https://x.com/_abhiii" },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/abhishekkambli/" },
+  { label: "Resume", href: "https://media.pixels.studio/abhishek-kambli-resume.pdf" },
+  { label: "Instagram", href: "https://www.instagram.com/pixels.studio.design/" },
+  { label: "Savee", href: "https://savee.com/abhi/" },
+] as const;
